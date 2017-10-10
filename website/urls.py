8c7b7e134ef8ac844dc893f3194031b86319e7c6
nam20485 <test@LM18.2>
2017-10-09 18:08:48 -0700

    add include import to fix error

--- website/urls.py
+++ website/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
+from django.conf.urls import url, include
 from django.contrib import admin
+#from django.contrib.auth.models import User
+#from rest_framework import routers, serializers, viewsets
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
