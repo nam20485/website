bf5ba628c65a150afe61322219433edb8e89dd5c
nam20485 <test@LM18.2>
2017-10-09 18:33:33 -0700

    test travis0ci

--- website/urls.py
+++ website/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     #url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    
 ]
