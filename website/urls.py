da98861cd547a2414fa0ef95e119d561532e718d
nam20485 <test@LM18.2>
2017-10-09 18:15:27 -0700

    remove routers

--- website/urls.py
+++ website/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from rest_framework import routers, serializers, viewsets
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    #url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
