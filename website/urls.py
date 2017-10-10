096b55facd74d8b6bc0be812ce7c4bdaab0af356
nam20485 <test@LM18.2>
2017-10-09 18:14:15 -0700

    include routers

--- website/urls.py
+++ website/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 #from django.contrib.auth.models import User
-#from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets
 
 urlpatterns = [
     url(r'^', include(router.urls)),
